485147e0b6ffb63109f4f44515151e7fe55af096
Don Kruger <58859778+donkruger@users.noreply.github.com>
2023-03-25 17:39:27 +0200

    Update contact-us.js

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,1 +1,12 @@
 
+import Layout from '../components/Layout';
+
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Lucido V2</h1>
+    </Layout>
+  );
+};
+
+export default Contact;
