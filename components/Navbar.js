c44150e80066816dcd622d7612f432f9e766867a
Don Kruger <58859778+donkruger@users.noreply.github.com>
2023-03-25 17:35:55 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
 
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/how-it-works">
+            <a>How it works</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/investors">
+            <a>Investors</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact-us">
+            <a>Contact us</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
