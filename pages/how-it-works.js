0376155ca466fa2fdbe0180b126b2a24e6a0fee8
Don Kruger <58859778+donkruger@users.noreply.github.com>
2023-03-25 17:51:06 +0200

    Update how-it-works.js

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -1,1 +1,11 @@
+// pages/how-it-works.js
+
+export default function HowItWorks() {
+  return (
+    <div>
+      <h1>How it works</h1>
+      <p>Here you can add content about how Lucido V2 works.</p>
+    </div>
+  );
+}
 
