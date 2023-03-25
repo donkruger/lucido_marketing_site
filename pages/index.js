07d46d4c7aa9d5530159c7db885a8b11e1a8a338
Don Kruger <58859778+donkruger@users.noreply.github.com>
2023-03-25 17:38:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Layout from '../components/Layout';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <h1>Welcome to Lucido V2</h1>
+    </Layout>
+  );
+};
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+export default Home;
